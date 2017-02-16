5d76fa46d6e4a996b84da55813f3e34c76200994
jianlong108 <wlong108@iCloud.com>
2017-02-16 21:01:06 +0800

    init project

---
+++ BusinessApp/BusinessApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BusinessApp
+//
+//  Created by Wangjianlong on 2017/2/16.
+//  Copyright © 2017年 JL.Com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BusinessApp/BusinessApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BusinessApp
+//
+//  Created by Wangjianlong on 2017/2/16.
+//  Copyright © 2017年 JL.Com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
